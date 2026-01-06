ef0720d165eaeb281c8b1b6598205a499d3eba48
zndk-hys <hysryt+zndk@gmail.com>
2026-01-06 17:16:40 +0900

    ディレクトリ整理

--- src/prisma/postgres.ts
+++ src/prisma/postgres.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from './generated/prisma/postgres/client';
+import { PrismaClient } from '../generated/prisma/postgres/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 import pg from 'pg';
 
